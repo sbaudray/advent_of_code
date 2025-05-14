555e9a6689cbcb97c1799b87e732f7bbca7e599b
sbaudray <22913636+sbaudray@users.noreply.github.com>
2025-05-14 20:21:01 +0200

    2015:01:02

--- 2015/01.js
+++ 2015/01.js
@@ -15,7 +15,7 @@ const input = require("./01.input.json");
 // ))) and )())()) both result in floor -3.
 // To what floor do the instructions take Santa?
 
-(function main() {
+function part1() {
   let floor = 0;
 
   for (const char of input) {
@@ -26,5 +26,36 @@ const input = require("./01.input.json");
     }
   }
 
-  console.log(floor);
-})();
+  return floor;
+}
+
+// Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
+
+// For example:
+
+// ) causes him to enter the basement at character position 1.
+// ()()) causes him to enter the basement at character position 5.
+// What is the position of the character that causes Santa to first enter the basement?
+
+function part2() {
+  let floor = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    const char = input.charAt(i);
+
+    if (char === "(") {
+      floor += 1;
+    } else if (char === ")") {
+      floor -= 1;
+    }
+
+    if (floor === -1) {
+      return i + 1;
+    }
+  }
+
+  return -1;
+}
+
+console.log(part1());
+console.log(part2());
