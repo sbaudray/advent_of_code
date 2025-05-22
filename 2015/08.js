8f8843c7aa2135e1050f29d359d0c309a97b5598
sbaudray <22913636+sbaudray@users.noreply.github.com>
2025-05-22 10:51:44 +0200

    2015:08:02

--- 2015/08.js
+++ 2015/08.js
@@ -1,6 +1,5 @@
 import fs from "node:fs/promises";
 import path from "node:path";
-import { escape } from "node:querystring";
 
 const input = await fs.readFile(
   path.join(import.meta.dirname, "./08.input.txt"),
@@ -9,26 +8,47 @@ const input = await fs.readFile(
 
 const lines = input.split("\n");
 
-export function deltaBetweenCodeCharactersAndValueCharacters(line) {
-  const escaped = line
+function stringify(str) {
+  return '"' + str.replace(/\\/g, "\\\\").replace(/"/g, '\\"') + '"';
+}
+
+function parse(str) {
+  return str
+    .replace(/\\"/g, '"')
+    .replace(/\\\\/g, "\\")
     .replace(/\\x([0-9a-fA-F]{2})/g, (_match, hex) => {
       return String.fromCodePoint(parseInt(hex, 16));
     })
-    .replace(/\\"/g, '"')
-    .replace(/\\\\/g, "\\")
     .slice(1, -1);
+}
 
-  return line.length - escaped.length;
+function deltaBetweenRawStringAndParsedString(line) {
+  return line.length - parse(line).length;
+}
+
+function deltaBetweenStringifiedStringAndRawString(line) {
+  return stringify(line).length - line.length;
 }
 
 function part1(lines) {
   let delta = 0;
 
   for (const line of lines) {
-    delta += deltaBetweenCodeCharactersAndValueCharacters(line);
+    delta += deltaBetweenRawStringAndParsedString(line);
   }
 
   return delta;
 }
 
-console.log(part1(lines));
+function part2(lines) {
+  let delta = 0;
+
+  for (const line of lines) {
+    delta += deltaBetweenStringifiedStringAndRawString(line);
+  }
+
+  return delta;
+}
+
+console.log({ part1: part1(lines) });
+console.log({ part2: part2(lines) });
