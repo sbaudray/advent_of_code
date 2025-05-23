a64563fd91d64a4c7242c478f2c5a611fea4a625
sbaudray <22913636+sbaudray@users.noreply.github.com>
2025-05-23 12:23:50 +0200

    2015:10:01

---
+++ 2015/10.js
@@ -0,0 +1,36 @@
+const input = require("./10.input.json");
+
+function lookAndSay(input, n = 1) {
+  let previousNumber = input[0];
+  let previousNumberCount = 0;
+  let result = "";
+
+  for (let i = 0; i < input.length; i += 1) {
+    const char = input[i];
+
+    if (char !== previousNumber) {
+      result += `${previousNumberCount}${previousNumber}`;
+      previousNumberCount = 0;
+    }
+
+    previousNumber = char;
+    previousNumberCount += 1;
+
+    if (i === input.length - 1) {
+      result += `${previousNumberCount}${previousNumber}`;
+      previousNumberCount = 0;
+    }
+  }
+
+  if (n - 1 === 0) {
+    return result;
+  }
+
+  return lookAndSay(result, n - 1);
+}
+
+function part1() {
+  return lookAndSay(input, 40).length;
+}
+
+console.log({ part1: part1() });
