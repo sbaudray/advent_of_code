4ab1980c192e2982912889738d897b90308d4fa7
sbaudray <22913636+sbaudray@users.noreply.github.com>
2025-05-26 11:16:17 +0200

    2015:12:02

--- 2015/12.js
+++ 2015/12.js
@@ -1,27 +1,40 @@
 import input from './12.input.json' with { type: 'json'}
 
-function traverseWithCount(structure, sum = 0) {
+function traverseWithSum(structure, options = {}) {
+  if (Array.isArray(structure)) {
+    let sum = 0;
+    for (const struct of structure) {
+      sum += traverseWithSum(struct, options);
+    }
+
+    return sum;
+  }
+
   if (typeof structure === "string") {
     return 0;
   }
 
-  if (typeof structure === "number" && structure !== NaN) {
+  if (typeof structure === "number") {
     return structure;
   }
 
-  if (Array.isArray(structure)) {
-    for (const struct of structure) {
-      sum += traverseWithCount(struct);
-    }
-  } else if (typeof structure === "object" && structure !== null) {
-    sum += traverseWithCount(Object.values(structure));
-  }
+  if (typeof structure === "object") {
+    const values = Object.values(structure);
 
-  return sum;
+    if (options.ignoreObjectsWithRed && values.includes("red")) {
+      return 0;
+    }
+
+    return traverseWithSum(Object.values(structure), options);
+  }
 }
 
 function part1(input) {
-  return traverseWithCount(input);
+  return traverseWithSum(input);
 }
 
-console.log({ part1: part1(input) });
+function part2(input) {
+  return traverseWithSum(input, { ignoreObjectsWithRed: true });
+}
+
+console.log({ part1: part1(input), part2: part2(input) });
