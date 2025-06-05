fe3c9c979b0d377c8a07ea923778feccb14c6257
sbaudray <22913636+sbaudray@users.noreply.github.com>
2025-06-05 10:19:27 +0200

    2015:20:02

--- 2015/20.js
+++ 2015/20.js
@@ -1,32 +1,54 @@
-import INPUT from './20.input.json' with { type: 'json' }
+import INPUT from './20.input.json' with { type: 'json' };
 
-function* houseGenerator() {
-  let house = 1;
+// function* houseGenerator() {
+//   let house = 1;
 
-  while (true) {
-    let presents = 0;
+//   while (true) {
+//     let presents = 0;
 
-    for (let i = 1; i <= house; i++) {
-      if (house % i === 0) {
-        presents += i * 10;
-      }
+//     for (let i = 1; i <= house; i++) {
+//       if (house % i === 0) {
+//         presents += i * 10;
+//       }
+//     }
+
+//     if (presents >= INPUT) {
+//       yield { ok: true, number: house };
+//     }
+
+//     house++;
+//   }
+// }
+
+// // THIS IS SUPER SLOW APPROACH !!!
+// function lowestHouseNumberPart1() {
+//   for (const house of houseGenerator()) {
+//     if (house?.ok) {
+//       return house.number;
+//     }
+//   }
+// }
+
+function lowestHouseNumberPart2() {
+  const houses = Array(INPUT / 20).fill(0);
+
+  for (let elf = 1; elf < houses.length; elf++) {
+    let housesVisited = 0;
+
+    for (let i = elf; i < houses.length && housesVisited < 50; i += elf) {
+      houses[i] += elf * 11;
+      housesVisited += 1;
     }
-
-    if (presents >= INPUT) {
-      yield { ok: true, number: house };
-    }
-
-    house++;
   }
+
+  for (let i = 0; i < houses.length; i++) {
+    if (houses[i] >= INPUT) {
+      return i;
+    }
+  }
+
+  return -1;
 }
 
-// THIS IS SUPER SLOW APPROACH !!!
-function lowestHouseNumber() {
-  for (const house of houseGenerator()) {
-    if (house?.ok) {
-      return house.number;
-    }
-  }
-}
-
-console.log({ part1: lowestHouseNumber() });
+// console.log({ part1: lowestHouseNumberPart1() });
+console.log({ part2: lowestHouseNumberPart2() });
