4e27a80d055d16fbf7190be769fee77bf7e45cb1
sbaudray <22913636+sbaudray@users.noreply.github.com>
2025-06-15 14:56:15 +0200

    2016:03:02

--- 2016/03.js
+++ 2016/03.js
@@ -6,18 +6,47 @@ const input = await fs.readFile(
   "utf8"
 );
 
-const TRIANGLES = input.split("\n").map((line) => {
+const lines = input.split("\n");
+
+const TRIANGLES = lines.map((line) => {
   return line.match(/\d+/g).map((x) => parseInt(x, 10));
 });
 
-let possible = 0;
+const TRIANGLES_BY_COLUMN = [];
 
-for (const triangle of TRIANGLES) {
-  const sorted = triangle.sort((a, b) => a - b);
-
-  if (sorted[0] + sorted[1] > sorted[2]) {
-    possible += 1;
-  }
+for (let i = 0; i < TRIANGLES.length; i += 3) {
+  TRIANGLES_BY_COLUMN.push([
+    TRIANGLES[i][0],
+    TRIANGLES[i + 1][0],
+    TRIANGLES[i + 2][0],
+  ]);
+  TRIANGLES_BY_COLUMN.push([
+    TRIANGLES[i][1],
+    TRIANGLES[i + 1][1],
+    TRIANGLES[i + 2][1],
+  ]);
+  TRIANGLES_BY_COLUMN.push([
+    TRIANGLES[i][2],
+    TRIANGLES[i + 1][2],
+    TRIANGLES[i + 2][2],
+  ]);
 }
 
-console.log({ possible });
+function possible(triangles) {
+  let possible = 0;
+
+  for (const triangle of triangles) {
+    const sorted = triangle.sort((a, b) => a - b);
+
+    if (sorted[0] + sorted[1] > sorted[2]) {
+      possible += 1;
+    }
+  }
+
+  return possible;
+}
+
+console.log({
+  part1: possible(TRIANGLES),
+  part2: possible(TRIANGLES_BY_COLUMN),
+});
